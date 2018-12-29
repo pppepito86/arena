6d6c19e26f85b4041e6b43f94a83ad161efda229
Marin Shalamanov <shalamanov.marin@gmail.com>
2018-12-29 21:58:36 +0200

    displaying problem time and memory constraints

--- src/main/webapp/app/shared/model/problem.model.ts
+++ src/main/webapp/app/shared/model/problem.model.ts
@@ -3,8 +3,17 @@ export interface IProblem {
     title?: string;
     directory?: string;
     version?: number;
+    time?: number;
+    memory?: number;
 }
 
 export class Problem implements IProblem {
-    constructor(public id?: number, public title?: string, public directory?: string, public version?: number) {}
+    constructor(
+        public id?: number,
+        public title?: string,
+        public directory?: string,
+        public version?: number,
+        public time?: number,
+        public memory?: number
+    ) {}
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -120,7 +120,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
         new MomentLocalesPlugin({
             localesToKeep: [
                     'en',
-                    'fr'
+                    'bg'
                     // jhipster-needle-i18n-language-moment-webpack - JHipster will add/remove languages in this array
                 ]
         }),
