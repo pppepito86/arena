da02729fc850c8c4b08522200c142b73c61b6feb
Marin Shalamanov <shalamanov.marin@gmail.com>
2020-03-17 03:36:38 +0100

    Fix update script to use less memory

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,9 +6,10 @@ git stash
 git fetch
 git pull
 git stash apply
-killall java
+mvn clean
+npm run webpack:prod
 ./mvnw -Pprod -Dmaven.test.skip=true -DskipTests package
 cp ~/arena/src/arena/target/arena-0.0.1-SNAPSHOT.war ~/arena/arena.war
+killall java || true
 ~/arena/arena.war > ~/arena/stdout 2> ~/arena/stderr &
 popd
-
