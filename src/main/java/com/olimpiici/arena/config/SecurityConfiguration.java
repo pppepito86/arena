a868d1d4d1d0dbb9421297365843f2e11f3a0eba
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-06-11 02:28:15 +0300

    Adjust SecurityConfiguration

--- src/main/java/com/olimpiici/arena/config/SecurityConfiguration.java
+++ src/main/java/com/olimpiici/arena/config/SecurityConfiguration.java
@@ -106,6 +106,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/public/**").permitAll()
+            .antMatchers("/api/competitions/**").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
