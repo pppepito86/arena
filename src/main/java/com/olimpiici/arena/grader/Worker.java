386c10ca804b1ed4bae2712d8975b93d5e8cfd92
Marin Shalamanov <shalamanov.marin@gmail.com>
2022-01-28 16:24:30 +0100

    Better logging when the worker doesn't return status

--- src/main/java/com/olimpiici/arena/grader/Worker.java
+++ src/main/java/com/olimpiici/arena/grader/Worker.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.olimpiici.arena.service.SubmissionService;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -24,14 +21,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olimpiici.arena.service.SubmissionService;
+
 public class Worker {
-	
+
     private final Logger log = LoggerFactory.getLogger(Worker.class);
 
 	private ObjectMapper mapper = new ObjectMapper();
-	
+
 	private final String workDir;
-	
+
 	private String url;
 	private boolean isFree = true;
 
@@ -41,13 +41,13 @@ public class Worker {
 		if (url.endsWith("/"))
 			url = url.substring(0, url.length() - 1);
 		this.url = url;
-		
+
 		this.workDir = workDir;
 	}
 
 	public SubmissionScore grade(long problemId, long submissionId, GraderTask listener, boolean isAuthor)
 			throws Exception {
-		
+
 		if (!isProblemUploaded(problemId)) {
 			log.debug("Problem is not uloaded. Uploading...");
 			File problemZip = getProblemFile(problemId);
@@ -59,7 +59,7 @@ public class Worker {
                 .addBinaryBody("file", sourceFile, ContentType.TEXT_PLAIN, sourceFile.getName())
                 .addTextBody("metadata", "{\"problemId\":" + problemId + "}", ContentType.APPLICATION_JSON)
                 .build();
-		
+
         // if it's author solution run with a big TL
         String authorParam = isAuthor ? "?tl=20" : "";
 		HttpPost post = new HttpPost(url + "/api/v1/submissions/" + submissionId + authorParam);
@@ -68,7 +68,7 @@ public class Worker {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		httpclient.execute(post);
 		httpclient.close();
-		
+
 		for (int i = 0; i < 600; i++) {
 			SubmissionScore score = getScore(submissionId);
 			listener.updateScore(submissionId, score);
@@ -82,11 +82,11 @@ public class Worker {
 	private File getProblemFile(long problemId) {
 		return Paths.get(workDir, "problems", ""+problemId, "problem.zip").toFile();
 	}
-	
+
 	private File getSourceFile(long submissionId) {
 		return SubmissionService.findSubmissionFile(workDir, submissionId).get();
 	}
-	
+
 	private boolean isRunning(long submissionId) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet get = new HttpGet(url + "/api/v1/submissions/" + submissionId + "/status");
@@ -95,19 +95,37 @@ public class Worker {
 		httpclient.close();
 		return "running".equals(responseString);
 	}
-	
+
 	private SubmissionScore getScore(long submissionId) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet get = new HttpGet(url + "/api/v1/submissions/" + submissionId + "/score");
 		CloseableHttpResponse response = httpclient.execute(get);
-		String responseString = EntityUtils.toString(response.getEntity());
-		httpclient.close();
-		SubmissionScore score = mapper.readValue(responseString, SubmissionScore.class);
-		
-		log.debug("Response for " + submissionId + " is: " + response.getStatusLine() + ", points are: " + score.getScore());
-		return score;
+		try {
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode != 200) {
+				log.warn("The worker returned status code {} when checking score" +
+						" for submission {}. Reason {} ", statusCode, submissionId,
+						response.getStatusLine().getReasonPhrase());
+			}
+			HttpEntity entity = response.getEntity();
+			if (entity == null) {
+				log.warn("The worker returned null entity for submission {}",
+						statusCode, submissionId);
+				SubmissionScore score = new SubmissionScore();
+				score.addFinalScore("system error", 0);
+				return score;
+			}
+			String responseString = EntityUtils.toString(entity);
+			SubmissionScore score = mapper.readValue(responseString, SubmissionScore.class);
+
+			log.debug("Response for " + submissionId + " is: " + response.getStatusLine() + ", points are: " + score.getScore());
+			return score;
+		} finally {
+			response.close();
+			httpclient.close();
+		}
 	}
-	
+
 	public boolean isAlive() {
 		RequestConfig config = RequestConfig.custom()
 				  .setConnectTimeout(3000)
@@ -126,7 +144,7 @@ public class Worker {
 			return false;
 		}
 	}
-	
+
 	public boolean isProblemUploaded(long problemId) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpGet get = new HttpGet(url + "/api/v1/problems/" + problemId);
@@ -140,24 +158,24 @@ public class Worker {
         HttpEntity entity = MultipartEntityBuilder.create()
                 .addBinaryBody("file", problemFile, ContentType.TEXT_PLAIN, problemFile.getName())
                 .build();
-		
+
 		HttpPost post = new HttpPost(url + "/api/v1/problems/" + problemId);
 		post.setEntity(entity);
 
 		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
 			httpclient.execute(post);
-		} 
+		}
 
 	}
-	
+
 	public void deleteProblem(long problemId) throws IOException {
 		HttpDelete delete = new HttpDelete(url + "/api/v1/problems/" + problemId);
-		
+
 		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
 			httpclient.execute(delete);
 		}
 	}
-	
+
 	public void setFree(boolean isFree) {
 		this.isFree = isFree;
 	}
