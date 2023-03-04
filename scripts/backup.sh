d7a8b33fa59f67357581ae6f87807dae0da11461
Marin Shalamanov <shalamanov.marin@gmail.com>
2023-03-04 17:50:14 +0100

    update backup script to match the one in prod

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,17 @@
 curr_date=`date '+%Y-%m-%d'`
+
+mkdir ~/arena/workdir/problems_to_backup
+
 pushd ~/arena/workdir/problems
+find . -mindepth 1 -maxdepth 1 -mtime -60 -exec ln -s $(pwd)/{} ../problems_to_backup/{} \;
+popd
+
+pushd ~/arena/workdir/problems_to_backup
 aws s3 sync . s3://backup.arena.olimpiici.com/problems_${curr_date} --exclude "*" --include "*.zip"
 popd
+
+rm -r ~/arena/workdir/problems_to_backup
+
 
 pushd ~/arena
 mysqldump -u root arena_dev > database.sql      # password required
