331d24f85c3dad742754af4af3780b6841bc9eb2
Marin Shalamanov <shalamanov.marin@gmail.com>
2023-03-04 17:59:02 +0100

    fix wrong button label in admin view

--- src/main/java/com/olimpiici/arena/web/rest/SubmissionResource.java
+++ src/main/java/com/olimpiici/arena/web/rest/SubmissionResource.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
